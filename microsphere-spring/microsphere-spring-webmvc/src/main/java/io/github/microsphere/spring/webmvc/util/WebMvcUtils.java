b9b1e8e04e15997c47d79340fecfe7ad331ad409
Mercy Ma <mercyblitz@gmail.com>
2022-10-29 19:02:22 +0800

    Migration from Alibaba spring-webmvc-support to be microsphere-spring-webmvc

--- microsphere-spring/microsphere-spring-webmvc/src/main/java/io/github/microsphere/spring/webmvc/util/WebMvcUtils.java
+++ microsphere-spring/microsphere-spring-webmvc/src/main/java/io/github/microsphere/spring/webmvc/util/WebMvcUtils.java
@@ -1,10 +1,20 @@
 package io.github.microsphere.spring.webmvc.util;
 
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.i18n.LocaleContext;
 import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -14,14 +24,29 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.filter.RequestContextFilter;
 import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import static org.springframework.util.ReflectionUtils.findMethod;
+import static org.springframework.util.ReflectionUtils.invokeMethod;
+import static org.springframework.web.context.ContextLoader.CONTEXT_INITIALIZER_CLASSES_PARAM;
+import static org.springframework.web.context.ContextLoader.GLOBAL_INITIALIZER_CLASSES_PARAM;
 
 /**
  * Spring Web MVC 静态工具类
@@ -40,6 +65,39 @@ public abstract class WebMvcUtils {
 
     public static final Set<Class<? extends HttpMessageConverter<?>>> supportedConverterTypes;
 
+    /**
+     * The name of AbstractJsonpResponseBodyAdvice class which was present in Spring Framework since 4.1
+     */
+    public static final String ABSTRACT_JSONP_RESPONSE_BODY_ADVICE_CLASS_NAME =
+            "org.springframework.web.servlet.mvc.findWebApplicationContextMethod.annotation.AbstractJsonpResponseBodyAdvice";
+
+    /**
+     * Indicates current version of Spring Framework is 4.1 or above
+     */
+    private final static boolean ABSTRACT_JSONP_RESPONSE_BODY_ADVICE_PRESENT =
+            ClassUtils.isPresent(ABSTRACT_JSONP_RESPONSE_BODY_ADVICE_CLASS_NAME, WebMvcUtils.class.getClassLoader());
+
+    /**
+     * {@link RequestMappingHandlerMapping} Context name
+     */
+    private final static String REQUEST_MAPPING_HANDLER_MAPPING_CONTEXT_NAME = RequestMappingHandlerMapping.class.getName();
+
+    /**
+     * Any number of these characters are considered delimiters between
+     * multiple values in a single init-param String value.
+     *
+     * @see ContextLoader#INIT_PARAM_DELIMITERS
+     */
+    private static final String INIT_PARAM_DELIMITERS = ",; \t\n";
+
+    /**
+     * RequestContextUtils#findWebApplicationContext(HttpServletRequest, ServletContext) method
+     *
+     * @since Spring 4.2.1
+     */
+    private static final Method findWebApplicationContextMethod = findMethod(RequestContextUtils.class,
+            "findWebApplicationContext", HttpServletRequest.class, ServletContext.class);
+
     static {
         Set<Class<? extends HttpMessageConverter<?>>> converterTypes = new HashSet<>(3);
         converterTypes.add(MappingJackson2HttpMessageConverter.class);
@@ -51,7 +109,6 @@ public abstract class WebMvcUtils {
      * 获取当前 {@link HttpServletRequest} 对象
      * <p>
      * 默认情况，{@link HttpServletRequest} 是在 {@link RequestContextFilter} 初始化，从 Servlet HTTP 请求线程 {@link ThreadLocal} 中获取
-     * 不过这个行为会被 io.github.microsphere.framework.commons.infrastructure.autoconfigure.CommonWebMvcAutoConfiguration#requestContextFilter() Bean 覆盖，
      * {@link HttpServletRequest} 是从 {@link InheritableThreadLocal} 中获取，能够在子线程中获取。
      *
      * @return 返回当前 {@link HttpServletRequest} 对象，获取不到返回<code>null</code>
@@ -242,6 +299,171 @@ public abstract class WebMvcUtils {
     public static <T> T getHandlerMethodReturnValue(Method method) {
         HttpServletRequest request = getHttpServletRequest();
         return getHandlerMethodReturnValue(request, method);
+    }
+
+    /**
+     * Determine whether the Bean Type is present annotated by {@link ControllerAdvice}
+     *
+     * @param beanType Bean Type
+     * @return If {@link ControllerAdvice} bean type is present , return <code>true</code> , or <code>false</code>.
+     */
+    public static boolean isControllerAdviceBeanType(Class<?> beanType) {
+        return AnnotationUtils.findAnnotation(beanType, ControllerAdvice.class) != null;
+    }
+
+    /**
+     * Get the {@link WebApplicationContext} from {@link HttpServletRequest}
+     *
+     * @param request        {@link HttpServletRequest}
+     * @param servletContext {@link ServletContext}
+     * @return {@link WebApplicationContext}
+     * @throws IllegalStateException if no servlet-specific context has been found
+     * @see RequestContextUtils#getWebApplicationContext(ServletRequest)
+     * @see DispatcherServlet#WEB_APPLICATION_CONTEXT_ATTRIBUTE
+     */
+    public static WebApplicationContext getWebApplicationContext(HttpServletRequest request, ServletContext servletContext) {
+
+        WebApplicationContext webApplicationContext = null;
+
+        if (findWebApplicationContextMethod != null) {
+
+            try {
+
+                webApplicationContext = (WebApplicationContext)
+                        invokeMethod(findWebApplicationContextMethod, null, request, servletContext);
+
+            } catch (IllegalStateException e) {
+
+            }
+
+        }
+
+        if (webApplicationContext == null) {
+
+            webApplicationContext = RequestContextUtils.findWebApplicationContext(request, servletContext);
+
+        }
+
+        return webApplicationContext;
+
+    }
+
+    protected static String appendInitParameter(String existedParameterValue, String... parameterValues) {
+
+        String[] existedParameterValues = StringUtils.hasLength(existedParameterValue) ?
+                existedParameterValue.split(INIT_PARAM_DELIMITERS) :
+                new String[0];
+
+        List<String> parameterValuesList = new ArrayList<String>();
+
+        if (!ObjectUtils.isEmpty(existedParameterValues)) {
+            parameterValuesList.addAll(Arrays.asList(existedParameterValues));
+        }
+
+        parameterValuesList.addAll(Arrays.asList(parameterValues));
+
+        String newParameterValue = StringUtils.arrayToDelimitedString(parameterValuesList.toArray(), ",");
+
+        return newParameterValue;
+    }
+
+    /**
+     * Append {@link ServletContext#setInitParameter(String, String) ServletContext Intialized Parameters}
+     *
+     * @param servletContext  {@link ServletContext}
+     * @param parameterName   the name of init parameter
+     * @param parameterValues the values of init parameters
+     */
+    public static void appendInitParameters(ServletContext servletContext, String parameterName, String... parameterValues) {
+
+        Assert.notNull(servletContext);
+        Assert.hasLength(parameterName);
+        Assert.notNull(parameterValues);
+
+        String existedParameterValue = servletContext.getInitParameter(parameterName);
+
+        String newParameterValue = appendInitParameter(existedParameterValue, parameterValues);
+
+        if (StringUtils.hasLength(newParameterValue)) {
+            servletContext.setInitParameter(parameterName, newParameterValue);
+        }
+
+    }
+
+    /**
+     * Append  initialized parameter for {@link ApplicationContextInitializer Global Initializer Class}
+     *
+     * @param servletContext          {@link ServletContext}
+     * @param contextInitializerClass the class of {@link ApplicationContextInitializer}
+     * @see ContextLoader#GLOBAL_INITIALIZER_CLASSES_PARAM
+     */
+    public static void appendGlobalInitializerClassInitParameter(ServletContext servletContext,
+                                                                 Class<? extends ApplicationContextInitializer> contextInitializerClass) {
+
+        String contextInitializerClassName = contextInitializerClass.getName();
+
+        appendInitParameters(servletContext, GLOBAL_INITIALIZER_CLASSES_PARAM, contextInitializerClassName);
+
+    }
+
+    /**
+     * Append  initialized parameter for {@link ApplicationContextInitializer Context Initializer Class}
+     *
+     * @param servletContext          {@link ServletContext}
+     * @param contextInitializerClass the class of {@link ApplicationContextInitializer}
+     * @see ContextLoader#CONTEXT_INITIALIZER_CLASSES_PARAM
+     */
+    public static void appendContextInitializerClassInitParameter(ServletContext servletContext,
+                                                                  Class<? extends ApplicationContextInitializer> contextInitializerClass) {
+
+        String contextInitializerClassName = contextInitializerClass.getName();
+
+        appendInitParameters(servletContext, CONTEXT_INITIALIZER_CLASSES_PARAM, contextInitializerClassName);
+
+    }
+
+
+    /**
+     * Append initialized parameter for {@link ApplicationContextInitializer Context Initializer Class} into {@link
+     * FrameworkServlet}
+     *
+     * @param servletContext          {@link ServletContext}
+     * @param contextInitializerClass the class of {@link ApplicationContextInitializer}
+     * @see FrameworkServlet#applyInitializers(ConfigurableApplicationContext)
+     */
+    public static void appendFrameworkServletContextInitializerClassInitParameter(
+            ServletContext servletContext,
+            Class<? extends ApplicationContextInitializer> contextInitializerClass) {
+
+        Collection<? extends ServletRegistration> servletRegistrations =
+                WebUtils.findServletRegistrations(servletContext, FrameworkServlet.class).values();
+
+        for (ServletRegistration servletRegistration : servletRegistrations) {
+            String contextInitializerClassName = servletRegistration.getInitParameter(CONTEXT_INITIALIZER_CLASSES_PARAM);
+            String newContextInitializerClassName = appendInitParameter(contextInitializerClassName, contextInitializerClass.getName());
+            servletRegistration.setInitParameter(CONTEXT_INITIALIZER_CLASSES_PARAM, newContextInitializerClassName);
+        }
+
+    }
+
+    /**
+     * Is page render request
+     *
+     * @param modelAndView {@link ModelAndView}
+     * @return If current request is for page render , return <code>true</code> , or <code>false</code>.
+     */
+    public static boolean isPageRenderRequest(ModelAndView modelAndView) {
+
+        if (modelAndView != null) {
+
+            String viewName = modelAndView.getViewName();
+
+            return StringUtils.hasText(viewName);
+
+        }
+
+        return false;
+
     }
 
     private static String getHandlerMethodRequestBodyArgumentAttributeName(Method method) {
