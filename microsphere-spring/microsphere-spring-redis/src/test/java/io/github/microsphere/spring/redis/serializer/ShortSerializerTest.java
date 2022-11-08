1448502e74f66206fb8cf6533736a6b0f9317e66
Mercy Ma <mercyblitz@gmail.com>
2022-11-08 11:10:07 +0800

    Update ShortSerializerTest.java

--- microsphere-spring/microsphere-spring-redis/src/test/java/io/github/microsphere/spring/redis/serializer/ShortSerializerTest.java
+++ microsphere-spring/microsphere-spring-redis/src/test/java/io/github/microsphere/spring/redis/serializer/ShortSerializerTest.java
@@ -3,7 +3,7 @@ package io.github.microsphere.spring.redis.serializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 /**
- * {@link ShortSerializer} 测试
+ * {@link ShortSerializer} Test
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @since 1.0.0
